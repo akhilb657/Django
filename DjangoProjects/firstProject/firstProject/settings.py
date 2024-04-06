fcb8369b2dc7500cb88226a539ec4beb1cfa6056
akhil <akhilboyanapalli657@gmail.com>
2024-04-06 13:02:56 -0500

    corrected DB configurations accross projects

--- DjangoProjects/firstProject/firstProject/settings.py
+++ DjangoProjects/firstProject/firstProject/settings.py
@@ -77,10 +77,8 @@ WSGI_APPLICATION = 'firstProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'employeedb',
-        'USER': 'root',
-        'PASSWORD': 'test18'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
